96a7b07e5372befe1a0db50da9870c6df9d84246
dmolyboh <denys.molyboha@cosmonova.net>
2024-08-26 12:02:19 +0300

    create addone models UserSite VPNUsage

--- src/apps/vpn_service/models.py
+++ src/apps/vpn_service/models.py
@@ -9,3 +9,24 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class UserSite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    original_url = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+
+class VPNUsage(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    site = models.ForeignKey(UserSite, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    page_views = models.PositiveIntegerField(default=0)
+    data_sent = models.PositiveIntegerField(default=0)  # В байтах
+    data_received = models.PositiveIntegerField(default=0)  # В байтах
+
+    def __str__(self):
+        return f"Usage for {self.site.name} by {self.user.username}"
