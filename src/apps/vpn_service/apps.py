c1b3d9f1045c90620274a322803b0545bb37378d
dmolyboh <denys.molyboha@cosmonova.net>
2024-08-26 12:02:36 +0300

    delete signals

--- src/apps/vpn_service/apps.py
+++ src/apps/vpn_service/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class VpnServiceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.vpn_service'
-
-    def ready(self):
-        from .signals import create_user_profile, save_user_profile
