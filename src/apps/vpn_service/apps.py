cda971cb453f1ee0aceaa2f5ac02aa26f5eeb2cc
dmolyboh <denys.molyboha@cosmonova.net>
2024-08-26 11:09:42 +0300

    Add UserProfile model and signals for profile creation and update

--- src/apps/vpn_service/apps.py
+++ src/apps/vpn_service/apps.py
@@ -3,4 +3,7 @@ from django.apps import AppConfig
 
 class VpnServiceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'vpn_service'
+    name = 'apps.vpn_service'
+
+    def ready(self):
+        from .signals import create_user_profile, save_user_profile
